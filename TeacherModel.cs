b562a5ce652159cbd4082b385a55aa68d9415511
Vignesh970 <72601183+Vignesh970@users.noreply.github.com>
2021-01-12 13:58:41 +0530

    first commit

---
+++ TeacherModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp13
+{
+    class TeacherModel
+    {
+        public int Tid { get; set; }
+        public string Tname { get; set; }
+        public string Tclass { get; set; }
+        public string Tsection { get; set; }
+    }
+}
